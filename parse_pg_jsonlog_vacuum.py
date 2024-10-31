b5c9641316beae0f4027da3656908deac23ad530
Aleš Zelený <zeleny.ales@gmail.com>
2024-10-31 23:54:59 +0100

    Added vacuum entries examples and implemented parsing some of them. Time is parsed to allow future custom formatting.

--- parse_pg_jsonlog_vacuum.py
+++ parse_pg_jsonlog_vacuum.py
@@ -5,6 +5,7 @@ import sys
 import os
 import re
 import json
+from datetime import datetime
 
 log_entry_pattern = r'''
     (?P<log_entry>^
@@ -64,6 +65,14 @@ autovacuum_data_pattern = r'''automatic\svacuum\sof\stable\s+
                         .*
                     )
                 )
+            |
+                (?:\n\s*
+                    (?P<removable_cutoff>
+                        removable\s+cutoff:\s+(?:(?P<removable_cutoff_txid>\d+))
+                        (,\s+which\s+was\s+(?P<removable_cutoff_xids_age>\d+)\s+XIDs\s+old\s+when\s+operation\s+ended)?
+                        .*
+                    )
+                )
         )*
     )
 '''
@@ -83,13 +92,22 @@ autovacuum_data_pattern = r'''automatic\svacuum\sof\stable\s+
 
 
 def parse_autovacuum(autovacuum_log):
-    log_timestamp = autovacuum_log["timestamp"]
+    log_ts = datetime.strptime(autovacuum_log["timestamp"], log_ts_format)
+    log_timestamp = log_ts.strftime('%Y-%m-%d, %H, %M')
+    # log_timestamp = log_ts.strftime('%Y-%m-%d %H:%M:%S.%f %Z')
     autovacuum_data = re_autovacuum_data.search(autovacuum_log["message"])
     # print(type(autovacuum_log["message"]))
     # print(autovacuum_log["message"])
+    # print()
     if autovacuum_data:
         print(
-                '%s\t%s.%s.%s: index scans: %s, pages: ( %s removed, %s remain, %s scanned - %s %% of total), tuples: ( %s removed, %s remain, %s dead not removable )' % (
+                (
+                    "%s, %s.%s.%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s"
+                    # "%s\t%s.%s.%s: index scans: %s, "
+                    # "pages: ( %s removed, %s remain, %s scanned - %s %% of total), "
+                    # "tuples: ( %s removed, %s remain, %s dead not removable ), "
+                    # "removable cutoff: %s, which was %s XIDs old when operation ended"
+                ) % (
                 # '%s\t%s.%s.%s: index scans: %s, removed: %s pages, remain: %s pages, scanned: %s pages, removed %s tuples, unknown entries: %s' % (
                       log_timestamp
                     , autovacuum_data.group('dbname')
@@ -103,6 +121,8 @@ def parse_autovacuum(autovacuum_log):
                     , autovacuum_data.group('vacuum_tuples_removed')
                     , autovacuum_data.group('vacuum_tuples_remain')
                     , autovacuum_data.group('vacuum_tuples_dead_nr')
+                    , autovacuum_data.group('removable_cutoff_txid')
+                    , autovacuum_data.group('removable_cutoff_xids_age')
                     # , autovacuum_data.group('unknown')
                 )
         )
@@ -126,6 +146,7 @@ def main():
         os.dup2(devnull, sys.stdout.fileno())
         sys.exit(1)  # Python exits with error code 1 on EPIPE
 
+log_ts_format = "%Y-%m-%d %H:%M:%S.%f %Z"
 re_log_entry = re.compile(log_entry_pattern, re.MULTILINE | re.VERBOSE)
 re_log_autovacuum = re.compile(log_autovacuum_pattern)
 #re_autovacuum_data = regex.compile(autovacuum_data_pattern, re.MULTILINE)
@@ -134,3 +155,61 @@ vacuum_data = []
 
 if __name__ == '__main__':
     main()
+
+# # Vacuum log entries (pg16) examples:
+# automatic vacuum of table "xxx.yyy.zzz": index scans: 0
+# pages: 0 removed, 8566 remain, 589 scanned (6.88% of total)
+# tuples: 8 removed, 8871 remain, 524 are dead but not yet removable
+# removable cutoff: 289809785, which was 1298 XIDs old when operation ended
+# frozen: 5 pages from table (0.06% of total) had 14 tuples frozen
+# index scan bypassed: 11 pages from table (0.13% of total) have 11 dead item identifiers
+# I/O timings: read: 0.000 ms, write: 0.000 ms
+# avg read rate: 0.000 MB/s, avg write rate: 0.000 MB/s
+# buffer usage: 1223 hits, 0 misses, 0 dirtied
+# WAL usage: 13 records, 0 full page images, 1181 bytes
+# system usage: CPU: user: 0.00 s, system: 0.00 s, elapsed: 0.00 s
+
+# automatic vacuum of table "xxx.yyy.zzz": index scans: 1
+# pages: 0 removed, 590 remain, 115 scanned (19.49% of total)
+# tuples: 129 removed, 18502 remain, 92 are dead but not yet removable
+# removable cutoff: 289809785, which was 1327 XIDs old when operation ended
+# frozen: 1 pages from table (0.17% of total) had 34 tuples frozen
+# index scan needed: 40 pages from table (6.78% of total) had 428 dead item identifiers removed
+# index "pk_xxx": pages: 94 in total, 0 newly deleted, 0 currently deleted, 0 reusable
+# index "ux_xxx": pages: 2 in total, 0 newly deleted, 0 currently deleted, 0 reusable
+# index "ux_zzz": pages: 2 in total, 0 newly deleted, 0 currently deleted, 0 reusable
+# I/O timings: read: 0.000 ms, write: 0.000 ms
+# avg read rate: 0.000 MB/s, avg write rate: 4.469 MB/s
+# buffer usage: 425 hits, 0 misses, 1 dirtied
+# WAL usage: 149 records, 0 full page images, 10712 bytes
+# system usage: CPU: user: 0.00 s, system: 0.00 s, elapsed: 0.00 s
+
+# automatic vacuum of table "xxx.yyy.zzz": index scans: 0
+# pages: 0 removed, 268873 remain, 7845 scanned (2.92% of total)
+# tuples: 0 removed, 3589280 remain, 0 are dead but not yet removable
+# removable cutoff: 289809785, which was 1335 XIDs old when operation ended
+# frozen: 757 pages from table (0.28% of total) had 10390 tuples frozen
+# index scan not needed: 0 pages from table (0.00% of total) had 0 dead item identifiers removed
+# I/O timings: read: 18.272 ms, write: 0.000 ms
+# avg read rate: 48.657 MB/s, avg write rate: 242.241 MB/s
+# buffer usage: 15355 hits, 465 misses, 2315 dirtied
+# WAL usage: 3825 records, 2316 full page images, 18627556 bytes
+# system usage: CPU: user: 0.03 s, system: 0.00 s, elapsed: 0.07 s
+
+# automatic vacuum of table "xxx.yyy.zzz": index scans: 1
+# pages: 0 removed, 120 remain, 120 scanned (100.00% of total)
+# tuples: 145 removed, 649 remain, 0 are dead but not yet removable
+# removable cutoff: 289811113, which was 531 XIDs old when operation ended
+# new relfrozenxid: 286371019, which is 7110685 XIDs ahead of previous value
+# new relminmxid: 68673587, which is 7106 MXIDs ahead of previous value
+# frozen: 0 pages from table (0.00% of total) had 0 tuples frozen
+# index scan needed: 63 pages from table (52.50% of total) had 160 dead item identifiers removed
+# index "ix_xx1": pages: 11 in total, 0 newly deleted, 3 currently deleted, 3 reusable
+# index "ix_xx2": pages: 41 in total, 1 newly deleted, 28 currently deleted, 27 reusable
+# index "ix_xx3": pages: 5 in total, 0 newly deleted, 0 currently deleted, 0 reusable
+# index "pk_xxx": pages: 41 in total, 1 newly deleted, 28 currently deleted, 27 reusable
+# I/O timings: read: 0.000 ms, write: 0.000 ms
+# avg read rate: 0.000 MB/s, avg write rate: 174.164 MB/s
+# buffer usage: 562 hits, 0 misses, 21 dirtied
+# WAL usage: 219 records, 6 full page images, 15754 bytes
+# system usage: CPU: user: 0.00 s, system: 0.00 s, elapsed: 0.00 s
