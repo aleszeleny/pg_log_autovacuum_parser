f682b1aaff3be6ffc3abfbfd513bccead6e1cffc
Aleš Zelený <zeleny.ales@gmail.com>
2025-01-18 03:29:29 +0100

    jsonlog_better_regexp

--- parse_pg_jsonlog_vacuum.py
+++ parse_pg_jsonlog_vacuum.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+# regex is needed to support named subroutines in regular expressions
+# sudo yum install python3-regex.x86_64
+
 import sys
 import os
-import re
+import regex
 import json
 from datetime import datetime
 
@@ -41,53 +44,137 @@ log_entry_pattern = r'''
 '''
 
 log_autovacuum_pattern = r'automatic vacuum of table'
-autovacuum_data_pattern = r'''automatic\svacuum\sof\stable\s+
-    \"(?P<dbname>\w+)\.(?P<schemaname>\w+)\.(?P<tablename>\w+)\":\s+
-    (?P<vacuum_data>
-        (?P<vacuum_index_scans>index\sscans:\s+
-            (?P<index_scans>\d+).*
-        )
-        (?:
-            \n\s*
-                (?P<vacuum_pages>
-                    pages:\s+(?:(?P<vacuum_pages_removed>\d+)\s+removed)
-                    (,\s+(?P<vacuum_pages_remain>\d+)\s+remain)?
-                    (,\s+(?P<vacuum_pages_scanned>\d+)\s+scanned)?
-                    (\s+\((?P<vacuum_pages_scanned_pct>\d[^%]*)%\s+of\s+total\))*
-                    .*
-                )
-            |
-                (?:\n\s*
-                    (?P<vacuum_tuples>
-                        tuples:\s+(?:(?P<vacuum_tuples_removed>\d+)\s+removed)
-                        (,\s+(?P<vacuum_tuples_remain>\d+)\s+remain)?
-                        (,\s+(?P<vacuum_tuples_dead_nr>\d[^%]*)\s+are\s+dead\s+but\s+not\s+yet\s+removable)?
-                        .*
-                    )
-                )
-            |
-                (?:\n\s*
-                    (?P<removable_cutoff>
-                        removable\s+cutoff:\s+(?:(?P<removable_cutoff_txid>\d+))
-                        (,\s+which\s+was\s+(?P<removable_cutoff_xids_age>\d+)\s+XIDs\s+old\s+when\s+operation\s+ended)?
-                        .*
-                    )
-                )
-        )*
-    )
-'''
+autovacuum_data_pattern = r'''
+    (?(DEFINE)  # start DEFINE block
 
-            # |(?:\n\s*(?P<vacuum_tuples>tuples:\s+(?:(?P<vacuum_tuples_removed>\d+)\s+removed).*))
-            # |(?:\n\s*(?P<vacuum_idx_scan_bypassed>index\sscan\sbypassed:\s+.*))
-            # |(?:\n\s*(?P<vacuum_idx_scan_needed>index\sscan\sneeded:\s+.*))
-            # |(?:\n\s*(?P<vacuum_idx>index\s\"(?P<vacuum_idx_name>\w+)\":\s+.*))
-            # |(?:\n\s*(?P<vacuum_idx_scan_not_needed>index\sscan\snot\sneeded:\s+.*))
-            # |(?:\n\s*(?P<vacuum_io_timings>I/O\s+timings:\s+.*))
-            # |(?:\n\s*(?P<vacuum_avg_read_rate>avg\s+read\s+rate:\s+.*))
-            # |(?:\n\s*(?P<vacuum_buffer_usage>buffer\s+usage:\s+.*))
-            # |(?:\n\s*(?P<vacuum_wal_usage>WAL\susage:\s+.*))
-            # |(?:\n\s*(?P<vacuum_system_usage>system\s+usage:\s+.*))
-            # |(?:\n\s*(?P<unknown>[^:]+:\s+.*))
+      # pre-define float subroutine 
+      (?P<float>(\d+)(?:\.\g<-1>)?)
+
+    )           # end DEFINE block
+
+##### The regex paring log entry starts here #####
+
+    # automatic vacuum of table "live.tickets_settlement.q_tickets_settlement": index scans: 0
+    automatic\svacuum\sof\stable\s+\"(?P<dbname>\w+)\.(?P<schemaname>\w+)\.(?P<tablename>\w+)\":  # vacuum "header" entry start
+    \s+(?P<vacuum_data>  # vacuum entry detail data
+    # indexes info (included in the header line)
+    (?=.*
+        (?P<vacuum_index_scans>index\sscans:\s+(?P<index_scans>\d+))
+    )
+
+    # vacuum pages
+    #pages: 0 removed, 264 remain, 17 scanned (6.44% of total)
+    (?=.*\n\s*
+        (?P<vacuum_pages>
+            (?:pages:\s+(?P<vacuum_pages_removed>\d+)\s+removed)
+            (?:     ,\s+(?P<vacuum_pages_remain>\d+)\s+remain)?
+            (?:     ,\s+(?P<vacuum_pages_scanned>\d+)\s+scanned)?
+            (?:      \s+\((?P<vacuum_pages_scanned_pct>(?&float))%\s+of\s+total\))
+        )
+    )?
+
+    # vacuum tuples
+    #tuples: 0 removed, 143 remain, 116 are dead but not yet removable
+    (?=.*\n\s*
+        (?P<vacuum_tuples>
+            (?:tuples:\s+(?:(?P<vacuum_tuples_removed>\d+)\s+removed))
+            (?:      ,\s+(?P<vacuum_tuples_remain>\d+)\s+remain)?
+            (?:      ,\s+(?P<vacuum_tuples_dead_nr>\d+)\s+are\s+dead\s+but\s+not\s+yet\s+removable)?
+        )
+    )?
+
+    # vacuum index info
+    (?=.*\n\s*
+        (?P<vacuum_index_info>
+            (?:
+                index\s+scan\s+not\s+needed:\s+(?P<vacuum_idx_scan_nn_pages>\d+)\s+pages\s+from\s+table
+                \s+\((?P<vacuum_idx_frozen_pct>(?&float))%\s+of\s+total\)
+                \s+had\s+(?P<vacuum_idx_dead_ii_removed>\d+)\s+dead\s+item\s+identifiers\s+removed
+            )
+        )
+    )?
+
+    # WAL info
+    (?=.*\n\s*
+        (?P<vacuum_wal_usage>
+            WAL\susage:
+            (?: \s+(?P<vacuum_wal_usage_records>\d+)\s+records)
+            (?:,\s+(?P<vacuum_wal_usage_fpi>\d+)\s+full\s+page\s+images)?
+            (?:,\s+(?P<vacuum_wal_usage_bytes>\d+)\s+bytes)?
+        )
+    )?
+
+    # vaccuum relation frozen XID
+    (?=.*\n\s*
+        (?P<vacuum_tab_pages_freeze>
+            (?:
+                frozen:\s+(?P<vacuum_tab_frozen_pages>\d+)\s+pages
+                \s+from\s+table\s+\((?P<vacuum_frozen_pages_pct>(?&float))
+            )
+        )
+    )?
+
+    # vaccuum xid cutoff
+    (?=.*\n\s*
+        (?P<vacuum_removable_cutoff>
+            (?:removable\s+cutoff:\s+(?P<vacuum_removable_cutoff_txid>\d+))
+            (?:,\s+which\s+was\s+(?P<vacuum_removable_cutoff_xids_age>\d+)\s+XIDs\s+old\s+when\s+operation\s+ended)?
+        )
+    )?
+
+    # vaccuum relation frozen XID
+    (?=.*\n\s*
+        (?P<vacuum_relfrozenxid>
+            (?:new\s+relfrozenxid:\s+(?P<vacuum_relfrozenxid_xid>\d+))
+            (?:,\s+which\s+is\s+(?P<vacuum_relfrozenxid_xids_ahead>\d+)\s+XIDs\s+ahead\s+of\s+previous\s+value)?
+        )
+    )?
+
+    # vaccuum relation frozen MXXID
+    (?=.*\n\s*
+        (?P<vacuum_relminmxid>
+            (?:new\s+relminmxid:\s+(?P<vacuum_relminmxid_xid>\d+))
+            (?:,\s+which\s+is\s+(?P<vacuum_relminmxid_xids_ahead>\d+)\s+MXIDs\s+ahead\s+of\s+previous\s+value)?
+        )
+    )?
+
+    # vacuum io timing
+    (?=.*\n\s*
+        (?P<vacuum_io_timing>
+            # it seems that both items are mandatory part of the line
+            (?:I\/O\s+timings:\s+(?:read:\s+)(?P<vacuum_io_read_ms>(?&float))\s+ms)
+            (?:,\s+(?:write:\s+)(?P<vacuum_io_write_ms>(?&float))\s+ms)
+        )
+    )?
+
+    # vacuum io rate
+    (?=.*\n\s*
+        (?P<vacuum_io_rate>
+            # it seems that both items are mandatory part of the line
+            (?:avg\s+read\s+rate:\s+(?P<vacuum_io_rate_read_mb>(?&float))\s+MB\/s)
+            (?:,\s+avg\s+write\s+rate:\s+(?P<vacuum_io_rate_write_mb>(?&float))\s+MB\/s)
+        )
+    )?
+
+    # vacuum buffer usage
+    (?=.*\n\s*
+        (?P<vacuum_buffer_usage>
+            (?:buffer\s+usage:\s+(?P<vacuum_buffer_usage_hits>\d+)\s+hits)
+            (?:,\s+(?P<vacuum_buffer_misses>\d+)\s+misses,\s+(?P<vacuum_buffer_usage_dirtied>\d+)\s+dirtied)
+        )
+    )?
+
+    # vacuum system usage
+    (?=.*\n\s*
+        (?P<vacuum_system_usage>
+            (?:system\s+usage:\s+CPU:\s+user:\s+(?P<vacuum_system_usage_user>(?&float))\s+s)
+            (?:,\s+system:\s+(?P<vacuum_system_usage_system>(?&float))\s+s)
+            (?:,\s+elapsed:\s+(?P<vacuum_system_usage_elapsed>(?&float))\s+s)
+        )
+    )?
+
+)
+'''
 
 def print_header():
     # initial_columns = ['timestamp']
@@ -106,6 +193,9 @@ def print_header():
             , 'vacuum_tuples_dead_nr'
             , 'removable_cutoff_txid'
             , 'removable_cutoff_xids_age'
+            , 'vacuum_wal_usage_records'
+            , 'vacuum_wal_usage_fpi'
+            , 'vacuum_wal_usage_bytes'
             ]
     print(', '.join(column_names))
 
@@ -120,13 +210,14 @@ def parse_autovacuum(autovacuum_log):
     if autovacuum_data:
         print(
                 (
-                    "%s, %s.%s.%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s"
+                    "%s, %s.%s.%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s"
+#                    "%s, %s.%s.%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s"
                     # "%s\t%s.%s.%s: index scans: %s, "
                     # "pages: ( %s removed, %s remain, %s scanned - %s %% of total), "
                     # "tuples: ( %s removed, %s remain, %s dead not removable ), "
                     # "removable cutoff: %s, which was %s XIDs old when operation ended"
+                    # WAL usage: %s records, %s full page images, %s bytes
                 ) % (
-                # '%s\t%s.%s.%s: index scans: %s, removed: %s pages, remain: %s pages, scanned: %s pages, removed %s tuples, unknown entries: %s' % (
                       log_timestamp
                     , autovacuum_data.group('dbname')
                     , autovacuum_data.group('schemaname')
@@ -139,8 +230,12 @@ def parse_autovacuum(autovacuum_log):
                     , autovacuum_data.group('vacuum_tuples_removed')
                     , autovacuum_data.group('vacuum_tuples_remain')
                     , autovacuum_data.group('vacuum_tuples_dead_nr')
-                    , autovacuum_data.group('removable_cutoff_txid')
-                    , autovacuum_data.group('removable_cutoff_xids_age')
+                    , autovacuum_data.group('vacuum_removable_cutoff_txid')
+                    , autovacuum_data.group('vacuum_removable_cutoff_xids_age')
+                    , autovacuum_data.group('vacuum_wal_usage_records')
+                    , autovacuum_data.group('vacuum_wal_usage_fpi')
+                    , autovacuum_data.group('vacuum_wal_usage_bytes')
+#                    , autovacuum_data.group('vacuum_data')
                     # , autovacuum_data.group('unknown')
                 )
         )
@@ -158,6 +253,7 @@ def main():
 
                 if ( log_data["backend_type"] == "autovacuum worker" ) and ( re_log_autovacuum.match(log_data["message"]) ):
                     # print(json.dumps(log_data, indent=2))
+                    # print(log_data["message"])
                     parse_autovacuum(log_data)
         sys.stdout.flush()
     except (BrokenPipeError, KeyboardInterrupt):
@@ -166,10 +262,11 @@ def main():
         sys.exit(1)  # Python exits with error code 1 on EPIPE
 
 log_ts_format = "%Y-%m-%d %H:%M:%S.%f %Z"
-re_log_entry = re.compile(log_entry_pattern, re.MULTILINE | re.VERBOSE)
-re_log_autovacuum = re.compile(log_autovacuum_pattern)
-#re_autovacuum_data = regex.compile(autovacuum_data_pattern, re.MULTILINE)
-re_autovacuum_data = re.compile(autovacuum_data_pattern, re.VERBOSE | re.MULTILINE)
+#re_log_entry = regex.compile(log_entry_pattern, regex.MULTILINE | regex.VERBOSE)
+re_log_entry = regex.compile(log_entry_pattern, regex.VERBOSE)
+re_log_autovacuum = regex.compile(log_autovacuum_pattern)
+#re_autovacuum_data = regex.compile(autovacuum_data_pattern, regex.MULTILINE)
+re_autovacuum_data = regex.compile(autovacuum_data_pattern, regex.VERBOSE | regex.MULTILINE | regex.DOTALL)
 vacuum_data = []
 
 if __name__ == '__main__':
